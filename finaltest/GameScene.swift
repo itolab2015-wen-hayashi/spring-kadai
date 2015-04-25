abfcd73c4f03d05fb36f8ed54dfa72d2caf611a8
betterteam <better@opossum.itolab.nitech.ac.jp>
2015-04-25 13:56:36 +0900

    fix the sizes

--- finaltest/GameScene.swift
+++ finaltest/GameScene.swift
@@ -16,7 +16,7 @@ let myBoundSizeStr: NSString = "Bounds width: \(myBoundSize.width) height: \(myB
 
 let TileSize:CGFloat = myBoundSize.width * 0.1
 
-let BoardLayerPosition = CGPointMake(0.12*myBoundSize.width, -0.2*myBoundSize.width)
+let BoardLayerPosition = CGPointMake(0.11*myBoundSize.width, -0.22*myBoundSize.width)
 let TextFieldPosition = CGPointMake(20, -20)
 let TextFieldPosition2 = CGPointMake(20, -60)
 
